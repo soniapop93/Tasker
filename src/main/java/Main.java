2390fd73630e1082ee95b7065057a7935f957c95
soniapop93 <popsonia85@gmail.com>
2023-12-11 23:24:28 +0200

    implemented function that returns the apps that are available at startup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
 import Logic.TaskerLogic;
+import ProcessListing.Startup;
 
 public class Main {
     public static void main(String[] args) {
-        TaskerLogic taskerLogic = new TaskerLogic();
-        taskerLogic.tasker();
+//        TaskerLogic taskerLogic = new TaskerLogic();
+//        taskerLogic.tasker();
+        Startup startup = new Startup();
+        startup.showStartupApps();
+
     }
 }
---
+++ src/main/java/ProcessListing/Startup.java
@@ -0,0 +1,26 @@
+package ProcessListing;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Startup {
+    public void showStartupApps() {
+        ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "wmic startup get caption,command");
+        builder.redirectErrorStream(true);
+
+        try {
+            Process process = builder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
